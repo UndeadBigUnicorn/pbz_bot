a4b68db871df22357cdc51b816a0dc4ee00e0c96
UndeadBigUnicorn <kravchel16@gmail.com>
2019-01-02 14:45:05 +0200

    initail commit

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
 import telebot
 import constants
+import cherrypy
 from telebot import types
 
 adminId = constants.adminId
-
 channelId = constants.channelId
 
+WEBHOOK_HOST = 'localhost'  # 'IP-адрес сервера, на котором запущен бот'
+WEBHOOK_PORT = 80  # 443, 80, 88 или 8443 (порт должен быть открыт!)
+WEBHOOK_LISTEN = '0.0.0.0'  # На некоторых серверах придется указывать такой же IP, что и выше
+
+WEBHOOK_SSL_CERT = './webhook_cert.pem'  # Путь к сертификату
+WEBHOOK_SSL_PRIV = './webhook_pkey.pem'  # Путь к приватному ключу
+
+WEBHOOK_URL_BASE = "https://%s:%s" % (WEBHOOK_HOST, WEBHOOK_PORT)
+WEBHOOK_URL_PATH = "/%s/" % (config.TOKEN)
 bot = telebot.TeleBot(constants.token)
 
 
